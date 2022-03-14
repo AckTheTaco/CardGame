f056d462cfed32479cf9b8ed3bf1ed21fd14b810
AckTheTaco <TonyC.burnett@gmail.com>
2022-03-14 13:46:18 -0700

    GameplaySetup
    Integrated the lists of the object s and you can cycle through them to select your gameplay experience. Next will be PlayerPrefs and creating a larger database for the mansions and scenarios

---
+++ UnityFile/GitCardGame/Assets/Scripts/GameplaySetup/Selection.cs
@@ -0,0 +1,125 @@
+using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+
+public class Selection : MonoBehaviour
+{
+   [SerializeField] private TMP_Text _selectedCharacter;
+   [SerializeField] private TMP_Text _selectedScenario;
+   [SerializeField] private TMP_Text _selectedMansion;
+
+
+   [SerializeField] private CharacterCollection _charactersDB;
+   [SerializeField] private ResourceCollectionBase _scenariosDB;
+   [SerializeField] private MansionDatabaseClass _mansionsDB;
+
+   [SerializeField] private List<CharacterClass> _characterList;
+
+   [SerializeField] private List<ResourceCollectionBase> _scenarioList;
+   [SerializeField] private List<MansionDatabaseClass> _mansionList;
+
+   private void Awake()
+   {
+      
+   }
+   private void Start()
+   {
+      _characterList = new List<CharacterClass>(_charactersDB.characterCollection);
+      _scenarioList.Add(_scenariosDB);
+      _mansionList.Add(_mansionsDB);
+
+      var name = _characterList[0].name;    
+      _selectedCharacter.text = name.Substring(name.IndexOf(" "));
+
+      _selectedScenario.text = _scenarioList[0].name;
+      _selectedMansion.text = _mansionList[0].name;
+   }
+
+   int index;
+   public void NextCharacter()
+   {
+      index++;
+      if ( (index+1) > _characterList.Count)
+      {  
+         index = 0;
+         
+      }
+      var name = _characterList[index].name;    
+      _selectedCharacter.text = name.Substring(name.IndexOf(" "));
+
+   }
+
+   public void PreviousCharacter()
+   {
+      index--;
+      if (index < 0)
+      {  
+         index = _characterList.Count-1;
+         
+      }
+      var name = _characterList[index].name;    
+      _selectedCharacter.text = name.Substring(name.IndexOf(" "));
+
+   }
+    public void PreviousScenario()
+   {
+      index--;
+      if (index < 0)
+      {  
+         index = _scenarioList.Count-1;
+         
+      }
+      var name = _scenarioList[index].name;    
+      _selectedScenario.text = name;
+
+   }
+
+    public void NextScenario()
+   {
+      index++;
+      if ( (index+1) > _scenarioList.Count)
+      {  
+         index = 0;
+         
+      }
+      var name = _scenarioList[index].name;    
+      _selectedScenario.text = name;
+
+   }
+
+
+   public void NextMansion()
+   {
+      index++;
+      if ( (index+1) > _mansionList.Count)
+      {  
+         index = 0;
+         
+      }
+      var name = _mansionList[index].name;    
+      _selectedMansion.text = name;
+
+   }
+
+    public void PreviousMansion()
+   {
+      index--;
+      if (index < 0)
+      {  
+         index = _mansionList.Count-1;
+         
+      }
+      var name = _mansionList[index].name;    
+      _selectedMansion.text = name;
+
+   }
+
+
+
+
+
+}
